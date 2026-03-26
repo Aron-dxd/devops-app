1ffeb96a706afe6d772cf06516e2d6925f977f34
Aron Thomas <aronthomas019@gmail.com>
2026-03-26 19:13:07 +0530

    Test ArgoCD automation

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     # We use an environment variable for the version so it's easy to change later
-    version = os.getenv("APP_VERSION", "v1")
-    return f"<h1>Hello World - {version}</h1>\n"
+    version = os.getenv("APP_VERSION", "v2")
+    return f"<h1>Iteration 1 Complete! - {version}</h1>\n"
 
 if __name__ == "__main__":
     # Standard web port 80. '0.0.0.0' allows external access.
